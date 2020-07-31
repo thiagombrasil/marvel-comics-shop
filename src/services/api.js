f648086f465c98c22e8080c3ccfa85737241e05e
Thiago Matos <thiago.matos256@gmail.com>
2020-07-31 11:56:37 -0400

    mudança na requisição para https

--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,6 @@ const publicKey = '3175c62bad6bef9f452c9ede0b181769';
 const privateKey = '7450d24ec088839ff96945e74877b3079583f7a5';
 const hash = md5(timestamp+privateKey+publicKey);
 
-const baseUrl = 'http://gateway.marvel.com/v1/public/comics';
+const baseUrl = 'https://gateway.marvel.com/v1/public/comics';
 
 export { timestamp, publicKey, hash, baseUrl };
